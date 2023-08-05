1eea4d5b5771990665440fbb4b50150cd99cda68
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-08-05 00:43:27 -0600

    misc improvements around types and FnBijection

--- core/src/main/scala/dev/librecybernetics/data/Bijection.scala
+++ core/src/main/scala/dev/librecybernetics/data/Bijection.scala
@@ -7,14 +7,34 @@ object Bijection:
     case Rebinding[A, B](rebinding: (A, B), current: (A, B)*)
   end Error
 
-  def apply[
-      A,
-      B,
-      F[_]: [F[_]] =>> MonadError[F, Bijection.Error],
-      T[_]: Traverse
-  ](
-      kvs: (A, B)*
-  ): F[MapBijection[A, B]] = MapBijection(kvs*)
+  /** Attempts to create a MapBijection from a sequence of correspondences. If any clash then it will return a Rebinding
+    * error
+    *
+    * ```
+    * Bijection('a' -> 1, 'b' -> 2, 'c' -> 3)
+    * ```
+    */
+  def apply[A, B, F[_]: [F[_]] =>> MonadError[F, Bijection.Error]](
+      correspondences: (A, B)*
+  ): F[MapBijection[A, B]] = MapBijection(correspondences*)
+
+  /** Creates a FnBijection from a forward and reverse function
+    *
+    * ```
+    * Bijection[Int, String](_.toString, _.toInt)
+    * ```
+    */
+  def apply[A, B](
+      forward: A => B,
+      reverse: B => A
+  ): FnBijection[A, B] = FnBijection(forward, reverse)
+
+  /** Creates a PFnBijection from a partial forward and reverse function
+    */
+  def apply[A, B](
+      forward: PartialFunction[A, B],
+      reverse: PartialFunction[B, A]
+  ): PFnBijection[A, B] = PFnBijection(forward, reverse)
 end Bijection
 
 trait Bijection[F[_, _]]:
--- core/src/main/scala/dev/librecybernetics/data/FnBijection.scala
+++ core/src/main/scala/dev/librecybernetics/data/FnBijection.scala
@@ -5,27 +5,53 @@ import scala.util.Try
 import cats.MonadError
 
 case class FnBijection[A, B](
-    forward: A => B,
-    reverse: B => A
+    forwardFn: A => B,
+    reverseFn: B => A
 )
 
-given Bijection[FnBijection] with
-  extension [A, B](fb: FnBijection[A, B])
+object FnBijection:
+  given Bijection[FnBijection] with
+    extension [A, B](fb: FnBijection[A, B])
+      // Properties
+      def isDefined(a: A): Boolean = Try(fb.forwardFn(a)).isSuccess
 
-    // Properties
-    def isDefined(a: A): Boolean = Try(fb.forward(a)).isSuccess
+      // Access
+      def apply(a: A): Option[B]   = Try(fb.forwardFn(a)).toOption
+      def reverse(b: B): Option[A] = Try(fb.reverseFn(b)).toOption
 
-    // Access
-    def apply(a: A): Option[B]   = Try(fb.forward(a)).toOption
-    def reverse(b: B): Option[A] = Try(fb.reverse(b)).toOption
+      // Transform
+      def flip: FnBijection[B, A] = FnBijection(fb.reverseFn, fb.forwardFn)
 
-    // Transform
-    def flip: FnBijection[B, A] = FnBijection(fb.reverse, fb.forward)
+      // Combine
+      @SuppressWarnings(Array("org.wartremover.warts.Throw"))
+      def ++[F[_]: [F[_]] =>> MonadError[F, Bijection.Error]](
+          other: FnBijection[A, B]
+      ): F[FnBijection[A, B]] =
+        MonadError[F, Bijection.Error].pure {
+          FnBijection(
+            forwardFn = a =>
+              (apply(a), other(a)) match
+                case (Some(b), None) => b
+                case (None, Some(b)) => b
 
-    // Combine
-    @SuppressWarnings(Array("org.wartremover.warts.TripleQuestionMark"))
-    def ++[
-        F[_]: [F[_]] =>> MonadError[F, Bijection.Error]
-    ](other: FnBijection[A, B]): F[FnBijection[A, B]] = ???
-  end extension
-end given
+                case (Some(b1), Some(b2)) if b1 == b2 => b1
+
+                case (_, _) =>
+                  throw new NoSuchElementException()
+              end match
+            ,
+            reverseFn = b =>
+              (reverse(b), other.reverse(b)) match
+                case (Some(a), None) => a
+                case (None, Some(a)) => a
+
+                case (Some(a1), Some(a2)) if a1 == a2 => a1
+
+                case (_, _) =>
+                  throw new NoSuchElementException()
+              end match
+          )
+        }
+    end extension
+  end given
+end FnBijection
